62d91fbad9ca4add81fad349c2605c596fa977bb
Shawn Ng <shawn@debian.huang>
2018-07-12 16:02:14 +0800

    fixed triggers

--- backend/triggers.py
+++ backend/triggers.py
@@ -16,7 +16,7 @@ class Trigger:
         df2 = pd.read_sql('unit', db.engine)
         df3 = pd.read_sql('pattern_full', db.engine)
         df1 = pd.merge(df1, df2, left_on='unit_id', right_on='id').drop(columns=['id_y', 'unit_id']).rename(index=str, columns={'id_x': 'id'})
-        df1 = pd.merge(df1, df3, left_on='pattern_id', right_on='id').drop(columns=['id_y', 'hours_per_tutorial', 'description', 'long_description', 'pattern_id']).rename(index=str, columns={'id_x': 'id', 'code_x': 'unit_code', 'code_y': 'pattern_code'}).sort_values('id')
+        df1 = pd.merge(df1, df3, left_on='pattern_id', right_on='id').drop(columns=['id_y', 'hour_per_tutorial', 'description', 'long_description', 'pattern_id']).rename(index=str, columns={'id_x': 'id', 'code_x': 'unit_code', 'code_y': 'pattern_code'}).sort_values('id')
         df1 = df1.assign(group_required = np.ceil(df1.enrolment / df1.student_per_group))
         df1['group_required'] = df1.group_required.astype(int)
         df1 = df1.assign(total_workload = df1.base + df1.tutorial * df1.group_required + df1.student * df1.enrolment)
