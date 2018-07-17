159f29b4c35ef4290c91b784c69bdc7fd09cdbb3
Shawn Ng <shawn@debian.huang>
2018-07-18 01:53:21 +0800

    test sent

--- backend/routes.py
+++ backend/routes.py
@@ -136,7 +136,7 @@ def edit_staff(staff_id):
 def edit_offering(offering_id):
     #update cache (redis) and then make a task queue to update postgres
     print("successfully reached checkpoint 1")
-    print(bool(request.json) == True)
+    print(request.json)
     if request.data and bool(request.json):
         content = request.json
         # offering = Offering.query.get(offering_id)
